a390ff2d8d6083e364dba7bdab816bfb2f0c77cb
kamalyusuboff <kamalyusuboff1905@gmail.com>
2026-08-12 17:24:36 +0400

    Navbar toggle in mobile JS.

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * navbar toggle in mobile
+ */
+
+const /** {NodeElement} */ $navbar = document.querySelector("[data-navbar]");
+const /** {NodeElement} */ $navToggler = document.querySelector("[data-nav-toggler]");
+
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
